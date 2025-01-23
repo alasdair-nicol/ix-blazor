fdad8d3110f7c67174e546fe019d5ca181a70107
Alasdair Nicol <alasdair.nicol@siemens.com>
2025-01-23 18:03:03 +0000

    Added sample project
    Project to provide an example of how the Blazor components can be used.
    Very basic, and doesn't really work well.

---
+++ SiemensIXBlazor.Sample/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,34 @@
+﻿using SiemensIXBlazor.Components;
+using SiemensIXBlazor.Objects.Application;
+
+namespace SiemensIXBlazor.Sample.Components.Layout;
+
+public partial class MainLayout
+{
+    private Application _app;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            AppSwitchConfig config = new()
+            {
+                CurrentAppId = "1",
+                Apps =
+                [
+                    new SiemensIXBlazor.Objects.Application.App()
+                    {
+                        Id = "App1",
+                        Name = "App 1",
+                        Description = "Awesome app",
+                        Url = "app1",
+                        Target = "_self",
+                        IconSrc = "logo.svg"
+                    }
+                ]
+            };
+
+            _app.AppSwitchConfig = config;
+        }
+    }
+}
